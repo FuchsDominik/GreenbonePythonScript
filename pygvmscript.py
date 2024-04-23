970327099305dc0a2d12d49c343595b3347a6c2c
Fuchs, Dominik <dominik.fuchs@adesso-service.com>
2024-04-23 13:13:18 +0200

    add tasks and targets

--- pygvmscript.py
+++ pygvmscript.py
@@ -9,7 +9,13 @@ connection = UnixSocketConnection(path=path)
 # using the with statement to automatically connect and disconnect to gvmd
 with Gmp(connection=connection) as gmp:
     # get the response message returned as a utf-8 encoded string
-    response = gmp.get_version()
+    response = gmp.get_targets()
 
     # print the response message
+    print('Targets: ')
+    print(response)
+
+    response = gmp.get_tasks()
+
+    print('Tasks: ')
     print(response)
